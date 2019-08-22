40b332cdd06cb79bc3b09bd9cdaf95b7700e1333
XudongFu <1666732060@qq.com>
2019-08-22 18:01:03 +0800

    Signed-off-by: XudongFu <1666732060@qq.com>

--- script/toweyeDetect.py
+++ script/toweyeDetect.py
@@ -1,69 +1,45 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt#约定俗成的写法plt
+#首先定义两个函数（正弦&余弦）
 import numpy as np
 from PIL import Image
-import math
 
 def getMatchPic(Left,right):
-    row=Left.shape[0]
-    empty = np.array(Image.new("RGB", (row, row)))
+    row,col=Left.shape
+    picwidth=50
+    width=picwidth+row
+    empty = np.array(Image.new("RGB", (width,width)))
+    empty[0:picwidth, 0:picwidth] = [255, 255, 255]
+    empty[picwidth:, picwidth:] = [255, 255, 255]
+    for x in range(width)[picwidth:]:
+        empty[x,0:picwidth]=Left[x-picwidth]
+        empty[0:picwidth,x]=right[x-picwidth]
     half=int(row/2)
-    empty[:,:] = [255, 255, 255]
-    for y in range(row)[half:]:
+    for y in range(row):
         for x in range(y):
             color=Left[y]-right[x]
-            if(color<0):
-                color=color*-1
-            empty[y,x]=[color,color,color]
-    for x in range(half):
-        for y in range(half):
-            color=Left[x]-right[y]
-            if(color<0):
-                color=color*-1
-            if (x <= y):
-                empty[x,y] =[color,color,color]
+            sum=0
+            for c in range(3):
+                sum=sum+ abs(color[c])
+            sum=sum/3
+            if(sum>2):
+                sum=255
+            empty[y+picwidth,x+picwidth]=[sum,sum,sum]
     plt.imshow(empty)
     pass
 
 
 path="F:/code/TwoEyeDetect/twoEyePic"
-pic="/home/free/roscode/TwoEyeDetect/twoEyePic/left1.png"
-picRight="/home/free/roscode/TwoEyeDetect/twoEyePic/right1.png"
+pic="F:/code/TwoEyeDetect/twoEyePic/left2.png"
+picRight="F:/code/TwoEyeDetect/twoEyePic/right2.png"
 left=np.array(Image.open(pic))
 right=np.array(Image.open(picRight))
 rows,cols,dims=left.shape
 half=int(rows/2)
-# dataX=range(rows)
-# dataY=left[half,:,0]
-# dataZ=left[half,:,1]
-# dataW=left[half,:,2]
-
-# plt.plot(dataX,dataY)
-# plt.plot(dataX,dataZ)
-# plt.plot(dataX,dataW)
-
-dataY=left[half,:,1]
-dataZ=right[half,:,1]
-
+dataX=range(rows)
+dataY=left[half,:,0]
+dataZ=left[half,:,1]
+dataW=left[half,:,2]
+dataY=left[half,:,:]
+dataZ=right[half,:,:]
 getMatchPic(dataY,dataZ)
-
-# plt.plot(dataX,dataY,'.')
-# plt.plot(dataX,dataZ,'.')
-# empty = np.array(Image.new("RGB",(rows,rows)))
-# empty[half:rows,0:half]=[255,255,255]
-#
-#
-# for x in range(half):
-#     for y in range(half):
-#         if(x<=y):
-#             empty[x,y] = [255, 255, 255]
-#
-# for x in range(rows)[half:]:
-#     for y in range(rows)[half:]:
-#         if(x>=y):
-#             empty[x,y] = [255, 255, 255]
-#
-# plt.imshow(empty)
-
-# plt.plot(dataX,dataY,"g-s")
-# plt.plot(dataX,dataZ,"g-s")
 plt.show()
